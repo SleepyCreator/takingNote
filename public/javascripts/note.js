e9037aa26ae63382673a7199cbbf583d38a39096
Viet <yukilugia@gmail.com>
2022-03-12 13:38:25 +0700

    go

---
+++ public/javascripts/note.js
@@ -0,0 +1,10 @@
+// const Header = require('@editorjs/header');
+// const editor = new EditorJS('editorjs');
+var editor = new EditorJS({
+
+    tools: {
+        header: Header,
+    },
+
+});
+// const editor = new EditorJS('editorjs');
